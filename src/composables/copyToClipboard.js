43dc1d1673995ded7184ebfdceb4fc7ef7b83cc8
Niklas Maroldt <nik.dev@gmx.net>
2026-02-08 16:46:46 +0100

    move copy timeout variable into copyToClipboard function

--- src/composables/copyToClipboard.js
+++ src/composables/copyToClipboard.js
@@ -2,9 +2,10 @@ import { ref } from 'vue';
 
 export function useClipboard() {
   const isCopied = ref(false);
-  let isCopiedTimeout = null;
 
   async function copyToClipboard(text) {
+    let isCopiedTimeout = null;
+
     try {
       await navigator.clipboard.writeText(text || window.location.href);
 
