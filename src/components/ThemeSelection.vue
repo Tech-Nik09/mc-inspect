7873b58d6278081338f7dd969043f03c5b2a4d70
Niklas Maroldt <nik.dev@gmx.net>
2026-02-21 19:20:26 +0100

    add theme selection

---
+++ src/components/ThemeSelection.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref, watch } from 'vue';
+
+const theme = ref(localStorage.getItem('theme') || 'auto');
+
+watch(
+  theme,
+  () => {
+    document.documentElement.setAttribute('data-theme', theme.value);
+    localStorage.setItem('theme', theme.value);
+  },
+  { immediate: true },
+);
+</script>
+
+<template>
+  <select v-model="theme">
+    <option value="auto">automatic</option>
+    <option value="light">light</option>
+    <option value="dark">dark</option>
+    <option value="oled">oled</option>
+  </select>
+
+  <p>Theme: {{ theme }}</p>
+</template>
+
+<style scoped></style>
