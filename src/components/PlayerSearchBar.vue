7c8d9ac20a50b8e60958f0e4fadc554cc3b0f9fa
Niklas Maroldt <nik.dev@gmx.net>
2026-03-09 19:25:01 +0100

    autofocus player search bar

--- src/components/PlayerSearchBar.vue
+++ src/components/PlayerSearchBar.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { usePlayerStore } from '@/stores/player';
+import { nextTick, onMounted, useTemplateRef, watch } from 'vue';
 import { useRouter } from 'vue-router';
 import { storeToRefs } from 'pinia';
 
@@ -8,15 +9,30 @@ const router = useRouter();
 const playerStore = usePlayerStore();
 const { isLoading, query } = storeToRefs(playerStore);
 
-async function onSearch(): Promise<void> {
+const searchBar = useTemplateRef('searchBar');
+
+async function onQuery(): Promise<void> {
   const newRoute = await playerStore.fetchPlayer();
   router.push(newRoute);
 }
+
+function focusInput(): void {
+  nextTick(() => {
+    if (!isLoading.value) searchBar.value?.focus();
+  });
+}
+onMounted(() => {
+  focusInput();
+});
+
+watch(isLoading, (loading) => {
+  if (!loading) focusInput();
+});
 </script>
 
 <template>
-  <input type="text" :disabled="isLoading" v-model="query" @keyup.enter="onSearch" />
-  <button :disabled="isLoading" @click="onSearch">{{ isLoading ? 'loading' : 'Search' }}</button>
+  <input type="text" :disabled="isLoading" @keyup.enter="onQuery" v-model="query" ref="searchBar" />
+  <button :disabled="isLoading" @click="onQuery">{{ isLoading ? 'loading' : 'Search' }}</button>
 </template>
 
 <style scoped></style>
