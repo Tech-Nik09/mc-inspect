6195c58caf420892c3f52474429566952330073d
Niklas Maroldt <nik.dev@gmx.net>
2026-03-13 16:11:49 +0100

    set up unplugin icons

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
+import Icons from 'unplugin-icons/vite';
+
 // https://vite.dev/config/
 export default defineConfig({
   server: {
@@ -22,7 +24,7 @@ export default defineConfig({
       },
     },
   },
-  plugins: [vue(), vueDevTools()],
+  plugins: [vue(), vueDevTools(), Icons({ compiler: 'vue3' })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
