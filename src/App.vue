c84469466981081443abca6eb412beff11d180ee
Niklas Maroldt <nik.dev@gmx.net>
2026-03-14 20:11:03 +0100

    refactor header and add loading indicator

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,12 @@
 <script setup lang="ts">
-import ThemeSelection from '@/components/ThemeSelection.vue';
+import Header from './components/Header.vue';
 </script>
 
 <template>
-  <header class="main-header">
-    <nav class="main-nav">
-      <router-link :to="{ name: 'home' }">Home</router-link>
-      <router-link :to="{ name: 'players' }">Players</router-link>
-      <router-link :to="{ name: 'playerInfo', params: { playerName: 'examplename' } }">Example player</router-link>
-      <router-link :to="{ path: '/does-not-exist' }">URL that doesn't exist</router-link>
-    </nav>
-
-    <ThemeSelection />
-  </header>
-
+  <Header />
   <main>
     <router-view></router-view>
   </main>
 </template>
 
-<style scoped>
-.main-header {
-  width: 100vw;
-  height: 4rem;
-
-  display: flex;
-  align-items: center;
-
-  border-bottom: 2px solid var(--primary-border-color);
-
-  transition: border-color var(--theme-transition);
-}
-
-.main-nav {
-  display: flex;
-  gap: 2rem;
-  align-items: center;
-}
-
-.theme-selection {
-  margin-left: auto;
-}
-</style>
+<style scoped></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,78 @@
+<script setup lang="ts">
+import { usePlayerStore } from '@/stores/player';
+import ThemeSelection from '@/components/ThemeSelection.vue';
+import { storeToRefs } from 'pinia';
+
+const playerStore = usePlayerStore();
+const { isLoading } = storeToRefs(playerStore);
+</script>
+
+<template>
+  <header class="main-header" :class="{ loading: isLoading }">
+    <nav class="main-nav">
+      <router-link :to="{ name: 'home' }">Home</router-link>
+      <router-link :to="{ name: 'players' }">Players</router-link>
+      <router-link :to="{ name: 'playerInfo', params: { playerName: 'examplename' } }">Example player</router-link>
+      <router-link :to="{ path: '/does-not-exist' }">URL that doesn't exist</router-link>
+    </nav>
+
+    <ThemeSelection />
+  </header>
+</template>
+
+<style scoped>
+.main-header {
+  width: 100vw;
+  height: 4rem;
+
+  display: flex;
+  align-items: center;
+
+  border-bottom: 2px solid var(--primary-border-color);
+
+  transition: border-color var(--theme-transition);
+}
+
+.main-nav {
+  display: flex;
+  gap: 2rem;
+  align-items: center;
+}
+
+.theme-selection {
+  margin-left: auto;
+}
+
+.loading {
+  position: relative;
+}
+
+.loading::after {
+  content: '';
+  position: absolute;
+  bottom: -2px;
+
+  height: 2px;
+
+  background-color: var(--primary-accent-color);
+
+  animation: loading 1s linear infinite alternate;
+}
+
+@keyframes loading {
+  0% {
+    left: 0;
+    width: 5%;
+  }
+
+  50% {
+    left: 30%;
+    width: 40%;
+  }
+
+  100% {
+    left: 95%;
+    width: 5%;
+  }
+}
+</style>
