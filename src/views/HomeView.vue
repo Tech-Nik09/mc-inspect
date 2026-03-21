b04e33b9f3fa7b0adfb66d8f6ac3c1bf8d85f01c
Niklas Maroldt <nik.dev@gmx.net>
2026-03-21 16:11:58 +0100

    update licensing notice in home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,16 +4,10 @@
   <h1>MC-Inspect</h1>
 
   <h2>Licensing</h2>
-  <p>This project uses third-party software components.</p>
-  <ul>
-    <li><a href="https://github.com/vuejs/core">vuejs</a> - <a href="https://github.com/vuejs/core?tab=MIT-1-ov-file">MIT license</a></li>
-    <li><a href="https://github.com/vuejs/router">vue-router</a> - <a href="https://github.com/vuejs/router?tab=MIT-1-ov-file">MIT license</a></li>
-    <li><a href="https://github.com/vuejs/pinia">pinia</a> - <a href="https://github.com/vuejs/pinia?tab=MIT-1-ov-file">MIT license</a></li>
-    <li>
-      <a href="https://github.com/bs-community/skinview3d">skinview3d</a> -
-      <a href="https://github.com/bs-community/skinview3d?tab=MIT-1-ov-file">MIT license</a>
-    </li>
-  </ul>
+  <p>
+    This project uses third-party software components and assets:
+    <a href="https://github.com/Tech-Nik09/mc-inspect/blob/main/README.md" target="_blank">click here</a>
+  </p>
 </template>
 
 <style scoped></style>
