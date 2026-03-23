131a8e7be1564755cfc61d717c9ec84eaad210e5
Niklas Maroldt <nik.dev@gmx.net>
2026-03-23 18:18:25 +0100

    install tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
+import tailwindcss from '@tailwindcss/vite';
 import Icons from 'unplugin-icons/vite';
 
 // https://vite.dev/config/
@@ -24,7 +25,7 @@ export default defineConfig({
       },
     },
   },
-  plugins: [vue(), vueDevTools(), Icons({ compiler: 'vue3' })],
+  plugins: [vue(), vueDevTools(), tailwindcss(), Icons({ compiler: 'vue3' })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
