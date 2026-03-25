382204cab9dcfb9b64414044cb7a7d561b0a79fe
Niklas Maroldt <nik.dev@gmx.net>
2026-03-25 06:38:27 +0100

    add dark mode support

--- src/components/ThemeSelection.vue
+++ src/components/ThemeSelection.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue';
+import { onMounted, onUnmounted, ref, watch } from 'vue';
 
 const themes = ['light', 'dark', 'auto'] as const;
 type Theme = (typeof themes)[number];
@@ -11,10 +11,18 @@ function isTheme(theme: any): theme is Theme {
 const savedTheme = localStorage.getItem('theme');
 const theme = ref<Theme>(isTheme(savedTheme) ? savedTheme : 'auto');
 
+const preferredThemeQuery = window.matchMedia('(prefers-color-scheme: dark)');
+const prefersDark = ref<boolean>(preferredThemeQuery.matches);
+function changePreferrsDark(event: MediaQueryListEvent): void {
+  prefersDark.value = event.matches;
+}
+onMounted(() => preferredThemeQuery.addEventListener('change', changePreferrsDark));
+onUnmounted(() => preferredThemeQuery.removeEventListener('change', changePreferrsDark));
+
 watch(
-  theme,
+  [theme, prefersDark],
   () => {
-    document.documentElement.setAttribute('data-theme', theme.value);
+    document.documentElement.classList.toggle('dark', theme.value === 'dark' || (theme.value === 'auto' && prefersDark.value));
     localStorage.setItem('theme', theme.value);
   },
   { immediate: true },
@@ -22,7 +30,7 @@ watch(
 </script>
 
 <template>
-  <input v-for="item in themes" :key="item" type="radio" :value="item" v-model="theme" />
+  <input v-for="option in themes" :key="option" type="radio" :value="option" v-model="theme" />
 
   <p>Theme: {{ theme }}</p>
 </template>
