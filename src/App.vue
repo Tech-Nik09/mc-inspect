7d5ea00d2673196d6314ae85e3bd9f2767daeeca
Niklas Maroldt <nik.dev@gmx.net>
2026-03-27 19:50:17 +0100

    remove placeholder router links

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,6 @@ import ThemeSelection from '@/components/ThemeSelection.vue';
     <div>
       <router-link :to="{ name: 'home' }">Home</router-link>
       <router-link :to="{ name: 'players' }">Players</router-link>
-      <router-link :to="{ name: 'playerInfo', params: { playerName: 'examplename' } }">Example player</router-link>
-      <router-link :to="{ path: '/does-not-exist' }">URL that doesn't exist</router-link>
     </div>
 
     <ThemeSelection />
