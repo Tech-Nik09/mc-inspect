10b888b82fb8e71aed0ec860263a88abe4f3c02e
Niklas Maroldt <nik.dev@gmx.net>
2026-04-01 11:13:28 +0200

    dynamically change favicon

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { usePlayerStore } from '@/stores/player';
 import HomeView from '../views/HomeView.vue';
 import { createRouter, createWebHistory, type RouteLocationNamedRaw, type RouteLocationNormalizedGeneric } from 'vue-router';
+import { useDateFormat, useNow } from '@vueuse/core';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -43,6 +44,7 @@ router.beforeEach(async (to) => {
 
 router.afterEach((to) => {
   setTitle(to);
+  setFavicon(to);
 });
 
 async function handlePlayerRoute(to: RouteLocationNormalizedGeneric): Promise<true | RouteLocationNamedRaw> {
@@ -63,6 +65,37 @@ function setTitle(to: RouteLocationNormalizedGeneric): void {
   if (to.name === 'playerInfo') routeTitle = (Array.isArray(to.params.playerName) ? to.params.playerName[0] : to.params.playerName) ?? null;
 
   document.title = routeTitle ? `${routeTitle} | ${baseTitle}` : baseTitle;
+}
+
+async function setFavicon(to: RouteLocationNormalizedGeneric): Promise<void> {
+  const version = useDateFormat(useNow(), 'YYYYMMDD');
+  const link = document.getElementById('dynamic-favicon');
+  if (!(link instanceof HTMLLinkElement)) return;
+
+  let icon = `/favicon.svg?v=${version.value}`;
+  let type = 'image/svg+xml';
+  let sizes: string | null = null;
+
+  if (to.name === 'playerInfo') {
+    const name = (Array.isArray(to.params.playerName) ? to.params.playerName[0] : to.params.playerName) ?? '';
+    const url = `https://render.crafty.gg/2d/head/${name}?size=96`;
+
+    try {
+      const res = await fetch(url, { method: 'HEAD' });
+      if (res.ok) {
+        icon = `${url}&v=${version.value}`;
+        type = 'image/png';
+        sizes = '96x96';
+      }
+    } catch (err) {
+      console.error(`Error while loading favicon from URL "${url}": ${err}`);
+    }
+  }
+
+  if (sizes) link.setAttribute('sizes', sizes);
+  else link.removeAttribute('sizes');
+  link.type = type;
+  link.href = icon;
 }
 
 export default router;
