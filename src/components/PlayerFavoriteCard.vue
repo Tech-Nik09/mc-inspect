0e8a212eb61b827f2fa6b2b36e8c3b6fd08e70a3
Niklas Maroldt <nik.dev@gmx.net>
2026-04-01 18:26:43 +0200

    change image in PlayerFavoriteCard

--- src/components/PlayerFavoriteCard.vue
+++ src/components/PlayerFavoriteCard.vue
@@ -12,7 +12,7 @@ const props = defineProps<{
 
 <template>
   <div @click="router.push({ name: 'playerInfo', params: { playerName: favorite.data.name } })">
-    <img :src="`https://vzge.me/head/64/${favorite.data.skinId}.webp?no=shadow`" alt="" />
+    <img :src="`https://render.crafty.gg/2d/head/${favorite.data.skinId}?size=96`" alt="" />
     <p>Name: {{ favorite.data.name }}</p>
     <p>UUID: {{ favorite.data.uuid }}</p>
     <FavoriteToggle @click.stop :id="favorite.meta.id" :type="favorite.meta.type" :data="favorite.data" />
