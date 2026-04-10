f3225e1e8109b7e39b8bc0d49cb9699ad67c0575
Niklas Maroldt <nm.codes@gmx.net>
2026-04-10 10:51:25 +0200

    refactor path reduction algorithm

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, watchEffect, type Ref } from 'vue';
+import { ref, watch } from 'vue';
 import { useRoute } from 'vue-router';
 
 const route = useRoute();
@@ -7,32 +7,39 @@ const route = useRoute();
 const maxLength: number = 20;
 const placeholder: string = '/...';
 
-let path: Ref<string>;
-watchEffect(() => {
-  path = ref(route.path);
-  if (path.value.length > maxLength) {
-    while (true) {
-      const segments = path.value.split('/');
-      segments.pop();
-      path.value = segments.join('/');
-      if (path.value.length <= maxLength - placeholder.length) {
-        path.value += placeholder;
-        break;
-      }
+const displayedPath = ref('');
+
+watch(
+  () => route.path,
+  (newPath) => {
+    if (newPath.length <= maxLength) {
+      displayedPath.value = newPath;
+      return;
     }
-  }
-});
+
+    let path = newPath;
+    const segments = path.split('/');
+
+    while (segments.length > 0 && path.length > maxLength - placeholder.length) {
+      segments.pop();
+      path = segments.join('/');
+    }
+
+    displayedPath.value = path + placeholder;
+  },
+  { immediate: true },
+);
 </script>
 
 <template>
-  <div class="flex flex-col items-center gap-4 pt-16">
+  <div class="mt-16 flex flex-col items-center gap-4">
     <h1 class="font-sans text-4xl font-bold">404</h1>
 
     <div class="flex w-full max-w-lg flex-col items-center gap-4 rounded-2xl border-2 border-accent p-4">
       <div class="text-center">
         <p>This page does not exist.</p>
         <p>
-          Path <span class="rounded-sm bg-slate-200 px-1 dark:bg-slate-800">{{ path }}</span> could not be found.
+          Path <span v-if="displayedPath" class="rounded-sm bg-slate-200 px-1 dark:bg-slate-800">{{ displayedPath }}</span> could not be found.
         </p>
       </div>
 
