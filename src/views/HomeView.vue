7eaaff8bc1d89896767676442df422e7fd8692d2
Niklas Maroldt <nm.codes@gmx.net>
2026-04-22 19:16:08 +0200

    style HomeView top section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,24 @@
 <template>
-  <h1>MC-Inspect</h1>
+  <section class="mt-8 mb-6 flex flex-col items-center gap-4 sm:mt-16 sm:mb-12">
+    <h1 class="font-sans text-4xl font-bold">MC-Inspect</h1>
 
-  <h2>Licensing</h2>
-  <p>
-    This project uses third-party software components and assets:
-    <a href="https://github.com/Tech-Nik09/mc-inspect/blob/main/README.md" target="_blank">click here</a>
-  </p>
+    <div class="flex w-full max-w-lg flex-col items-center gap-4 rounded-2xl border-2 border-accent p-4 shadow-center-md shadow-accent">
+      <div class="text-center">
+        <p>Welcome to MC-Inspect! A Minecraft player and server inspection web application.</p>
+      </div>
+
+      <nav class="contents">
+        <a href="https://github.com/nkmLABS/mc-inspect" target="_self" class="btn-primary w-full">View project on GitHub</a>
+      </nav>
+    </div>
+  </section>
+
+  <hr class="h-0.5 w-full bg-slate-400 text-slate-400 dark:bg-slate-600 dark:text-slate-600" />
+
+  <section class="mt-8 flex flex-col items-center gap-4 sm:mt-16">
+    <h2 class="font-sans text-2xl font-bold">About the project</h2>
+    <div>
+      <p>This is a demo project of nkmLABS where you can lookup various information about Minecraft players and servers.</p>
+    </div>
+  </section>
 </template>
