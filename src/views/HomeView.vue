62e2599d030b7dc38cb76ddafcdaa16a520eb616
Niklas Maroldt <nkmlabs.org@gmail.com>
2026-05-05 07:41:21 +0200

    limit HomeView content width

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -28,14 +28,14 @@ const cards: Card[] = [
 
   <hr class="h-0.5 w-full bg-slate-400 text-slate-400 dark:bg-slate-600 dark:text-slate-600" />
 
-  <section class="mt-8 flex max-w-3xl flex-col items-center gap-4 sm:mt-16">
+  <section class="mt-8 flex flex-col items-center gap-4 sm:mt-16">
     <h2 class="font-sans text-2xl font-bold">About the project</h2>
-    <p class="text-center text-balance sm:text-justify sm:text-wrap">
+    <p class="max-w-3xl text-center text-balance sm:text-justify sm:text-wrap">
       MC-Inspect lets you lookup various information about Minecraft players and servers. It is developed by nkmLABS as a learning exercise and now
       deployed as demo project for you to use. I (nkmLABS) build this web application to understand how to use different web technologies and tools.
       Especially, I acquired knowledge of:
     </p>
-    <div class="grid w-full auto-rows-fr grid-cols-1 gap-2 sm:grid-cols-2 sm:gap-4">
+    <div class="grid w-full max-w-3xl auto-rows-fr grid-cols-1 gap-2 sm:grid-cols-2 sm:gap-4">
       <div v-for="card in cards" :key="card.label" class="flex flex-col gap-2 rounded-2xl border-2 p-4 sm:gap-4 sm:p-8">
         <div class="flex items-center gap-2 sm:gap-4">
           <component :is="card.icon" class="size-8"></component>
