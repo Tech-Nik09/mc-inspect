fd95f71b230c940107f8c61c4631d287d5ff75d5
Niklas Maroldt <nkmlabs.org@gmail.com>
2026-05-08 05:30:40 +0200

    redefine copy button label to be optional

--- src/components/CopyButton.vue
+++ src/components/CopyButton.vue
@@ -1,14 +1,21 @@
 <script setup lang="ts">
+import { computed } from 'vue';
 import { useClipboard } from '@/composables/clipboard';
 
 const { isCopied, copyToClipboard } = useClipboard();
 
 const props = defineProps<{
   text: string;
-  label: string;
+  label?: string;
 }>();
+
+const buttonLabel = computed<string>(() => {
+  if (isCopied.value) return 'Copied';
+  if (props.label) return `Copy ${props.label}`;
+  return 'Copy';
+});
 </script>
 
 <template>
-  <button @click="copyToClipboard(text)">{{ isCopied ? 'Copied' : `Copy ${label}` }}</button>
+  <button @click="copyToClipboard(text)">{{ buttonLabel }}</button>
 </template>
